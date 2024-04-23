f19dd8d4750609e3fc4348c8ca8c36ec7daf60fd
narendra45 <narendranaru45@gmail.com>
2024-04-24 01:12:58 +0530

    adding curd application

---
+++ product/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from product.models import ProductDetails,Tags
+
+class TagsForm(forms.ModelForm):
+    class Meta:
+        model=Tags
+        fields=['tags_name']
+class ProductForms(forms.ModelForm):
+    #
+    # product_name=forms.CharField(max_length=30)
+    # tags=forms.ModelMultipleChoiceField(queryset=Tags.objects.all(),widget=forms.CheckboxSelectMultiple)
+    # image = forms.ImageField()
+    #
+    # CATEGORY_CHOICES = (
+    #     ('M', 'mobile'),
+    #     ('T', 'tv'),
+    #     ('G','grocery')
+    # )
+    # category = forms.ChoiceField(choices=CATEGORY_CHOICES)
+
+    class Meta:
+        model=ProductDetails
+        fields=['product_name','tags','image','category']
